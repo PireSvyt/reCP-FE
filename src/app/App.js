661dd6355160bbf8320e9f1bd7d93da9780ee3e2
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-03-28 15:51:39 +0200

    introducing Navbar

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Paper, BottomNavigation, BottomNavigationAction } from "@mui/material";
+
+import Navbar from "./Navbar";
 
 import Menu from "./menu";
 import navigation from "./navigation";
@@ -20,16 +23,35 @@ export default class App extends React.Component {
     return (
       <React.Fragment>
         <div id="menu" />
+        <div id="menu_placeholder" />
         {navigation.pages.map((page) => (
           <div id={`${page.code}`} />
         ))}
         <div id="navbar" />
+        <div id="navbar_placeholder" />
       </React.Fragment>
     );
   }
   componentDidMount() {
+    function dummy_navbar() {
+      return (
+        <Paper sx={{ bottom: 0, left: 0, right: 0 }} elevation={3}>
+          <BottomNavigation showLabels value={0}>
+            <BottomNavigationAction label="dummy navbar" />
+          </BottomNavigation>
+        </Paper>
+      );
+    }
+
     // Render sub components
     ReactDOM.render(<Menu />, document.getElementById("menu"));
+
+    ReactDOM.render(<Navbar />, document.getElementById("navbar"));
+    ReactDOM.render(
+      dummy_navbar(),
+      document.getElementById("navbar_placeholder")
+    );
+
     myrecipies.render();
     myingredients.render();
     recipeview.render();
@@ -37,11 +59,12 @@ export default class App extends React.Component {
     thisweek.render();
     myfridge.render();
     shopping.render();
-    navigation.render();
+
     // Update sub component content
     myrecipies.update();
     myingredients.update();
+
     // Navigate to landing page
-    navigation.navigates("thisweek");
+    //navigation.navigates("thisweek");
   }
 }
---
+++ src/app/Navbar.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { Paper, BottomNavigation, BottomNavigationAction } from "@mui/material";
+
+import navigation from "./navigation";
+
+export default function FixedBottomNavigation() {
+  const [value, setValue] = React.useState(3);
+
+  return (
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        {navigation.pages.map((page) => {
+          if (page.navbar === true) {
+            return (
+              <BottomNavigationAction
+                id={`${page.code}`}
+                label={`${page.label}`}
+                onClick={navigation.navigates(page.code)}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </BottomNavigation>
+    </Paper>
+  );
+}
