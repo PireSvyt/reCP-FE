39ba76e8ca6f59d49805a28d594ff57b277e5920
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-04-09 19:27:59 +0200

    in fragment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import React from "react";
 import ReactDOM from "react-dom";
 
 import Balance from "./app/balance/Balance";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Balance />, rootElement);
+ReactDOM.render(
+  <React.Fragment>
+    <Balance />
+  </React.Fragment>,
+  rootElement
+);
