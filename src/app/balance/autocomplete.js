03287f3ccfcdb365e2337fefe1c1a5633f749145
PireSvyt <81512700+PireSvyt@users.noreply.github.com>
2022-04-18 17:45:42 +0200

    cleaner ui

--- src/app/balance/autocomplete.js
+++ src/app/balance/autocomplete.js
@@ -2,73 +2,13 @@ import * as React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete, { createFilterOptions } from "@mui/material/Autocomplete";
 
+import config from "../../config";
+import appcopy from "./copy";
 import { getCategoryTransactions } from "./api/categorytransactions";
 
 const filter = createFilterOptions();
 var options = [];
 var currentValue = "";
-
-/*export function FreeSoloCreateOption() {
-  const [value, setValue] = React.useState(null);
-
-  return (
-    <Autocomplete
-      value={value}
-      onChange={(event, newValue) => {
-        if (typeof newValue === "string") {
-          setValue({
-            name: newValue
-          });
-        } else if (newValue && newValue.inputValue) {
-          // Create a new value from the user input
-          setValue({
-            name: newValue.inputValue
-          });
-        } else {
-          setValue(newValue);
-        }
-      }}
-      filterOptions={(options, params) => {
-        const filtered = filter(options, params);
-
-        const { inputValue } = params;
-        // Suggest the creation of a new value
-        const isExisting = options.some((option) => inputValue === option.name);
-        if (inputValue !== "" && !isExisting) {
-          filtered.push({
-            inputValue,
-            name: `Add "${inputValue}"`
-          });
-        }
-
-        return filtered;
-      }}
-      selectOnFocus
-      clearOnBlur
-      handleHomeEndKeys
-      id="free-solo-with-text-demo"
-      options={options}
-      getOptionLabel={(option) => {
-        // Value selected with enter, right from the input
-        if (typeof option === "string") {
-          return option;
-        }
-        // Add "xxx" option created dynamically
-        if (option.inputValue) {
-          return option.inputValue;
-        }
-        // Regular option
-        return option.name;
-      }}
-      renderOption={(props, option) => <li {...props}>{option.name}</li>}
-      sx={{ width: 300 }}
-      freeSolo
-      renderInput={(params) => (
-        <TextField {...params} label="Free solo with text demo" />
-      )}
-    />
-  );
-}*/
 
 export function CategorySelector() {
   //const [value, setValue] = React.useState(null);
@@ -141,7 +81,7 @@ export function CategorySelector() {
           id="balance_transaction_category_input"
           {...params}
           variant="standard"
-          label="CATEGORY"
+          label={appcopy["input.category"][config.app.language]}
         />
       )}
     />
